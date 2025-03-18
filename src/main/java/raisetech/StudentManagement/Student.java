dc9d98cec0d0671c9271e827ee4db2be9b434459
Fukuta Haruki <fukutaharuki@fukudaharukaonorenoMacBook-Air.local>
2025-03-19 05:21:06 +0900

    23_MyBatisを使ってCRUDのRead処理を実装と課題提出

--- src/main/java/raisetech/StudentManagement/Student.java
+++ src/main/java/raisetech/StudentManagement/Student.java
@@ -1,22 +1,18 @@
 package raisetech.StudentManagement;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Student {
+    private String id;
     private String name;
-    public int age;
+    private String furigana;
+    private String nickName;
+    private String email;
+    private String area;
+    private int age;
+    private String gender;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }

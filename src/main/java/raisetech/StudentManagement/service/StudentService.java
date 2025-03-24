5add710182cc8b23bc925c965ae9442b37959136
Fukuta Haruki <fukutaharuki@fukudaharukaonorenoMacBook-Air.local>
2025-03-24 17:58:13 +0900

    25_Read処理のConverter部分実装の課題提出

--- src/main/java/raisetech/StudentManagement/service/StudentService.java
+++ src/main/java/raisetech/StudentManagement/service/StudentService.java
@@ -7,7 +7,6 @@ import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentCourses;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class StudentService {
@@ -19,15 +18,11 @@ public class StudentService {
     }
 
     public List<Student> searchStudentList() {
-        List<Student> only30sai = repository.StudentListSearch()
-                .stream().filter(student -> student.getAge() >= 30 && student.getAge() <= 39).collect(Collectors.toList());
-        return only30sai;
+       return repository.StudentListSearch();
     }
 
     public List<StudentCourses> searchStudentCourseList() {
-        List<StudentCourses> onlyJavaCourse = repository.StudentCourseListSearch()
-                .stream().filter(student2 -> student2.getCourseName().matches("javaコース")).collect(Collectors.toList());
-        return onlyJavaCourse;
+        return repository.StudentCourseListSearch();
     }
 
 }

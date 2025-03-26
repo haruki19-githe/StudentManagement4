4270c27da738d47a9851267c6c152f5bc068cfc1
Fukuta Haruki <fukutaharuki@fukudaharukaonorenoMacBook-Air.local>
2025-03-26 16:29:06 +0900

    27_Thymeleafを使ったReadの画面描画処理の課題提出

--- src/main/java/raisetech/StudentManagement/controller/StudentController.java
+++ src/main/java/raisetech/StudentManagement/controller/StudentController.java
@@ -1,18 +1,18 @@
 package raisetech.StudentManagement.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import raisetech.StudentManagement.converter.StudentConverter;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentCourses;
-import raisetech.StudentManagement.domain.StudentDetail;
 import raisetech.StudentManagement.service.StudentService;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 import java.util.List;
 
-@RestController
+@Controller
 public class StudentController {
 
     private StudentService service;
@@ -25,11 +25,13 @@ public class StudentController {
     }
 
     @GetMapping("/studentList")
-    public List<StudentDetail> getStudentList() {
+    public String getStudentList(Model model) {
         List<Student> students = service.searchStudentList();
         List<StudentCourses> studentCourses = service.searchStudentCourseList();
 
-        return converter.convertStudentDetails(students, studentCourses);
+        model.addAttribute("studentList",converter.convertStudentDetails(students,studentCourses));
+        return "studentList";
+
     }
 
 

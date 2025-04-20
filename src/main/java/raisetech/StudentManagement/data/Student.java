0ffc07725503c5d86327349cf82a9522ec524d44
Fukuta Haruki <fukutaharuki@fukudaharukaonorenoMacBook-Air.local>
2025-04-21 01:56:51 +0900

    38_ドキュメントの必要性と作り方の課題提出

--- src/main/java/raisetech/StudentManagement/data/Student.java
+++ src/main/java/raisetech/StudentManagement/data/Student.java
@@ -1,24 +1,44 @@
 package raisetech.StudentManagement.data;
 //Model
 
-import jakarta.validation.constraints.NotBlank;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
+@Schema(description = "受講生")
 @Getter
 @Setter
 public class Student {
 
+    @Schema(description = "id", type = "string", example = "1")
     @Pattern(regexp = "^\\d+$")
     private String id;
+
+    @Schema(description = "名前", type = "string", example = "山田")
     private String name;
+
+    @Schema(description = "名前のフリガナ", type = "string", example = "ヤマダ")
     private String furigana;
+
+    @Schema(description = "名前のニックネーム", type = "string", example = "やまちゃん")
     private String nickName;
+
+    @Schema(description = "メールアドレス", type = "string", example = "taroYamada@emample.com")
     private String email;
+
+    @Schema(description = "住んでいる地域", type = "string", example = "北海道")
     private String area;
+
+    @Schema(description = "年齢", type = "int", example = "35")
     private int age;
+
+    @Schema(description = "性別", type = "string", example = "男性")
     private String gender;
+
+    @Schema(description = "備考欄", type = "string", example = "ご飯が好き")
     private String remark;
+
+    @Schema(description = "削除フラグ", type = "boolean")
     private boolean isDeleted;
 }
--- src/main/java/raisetech/StudentManagement/domain/StudentDetail.java
+++ src/main/java/raisetech/StudentManagement/domain/StudentDetail.java
@@ -1,6 +1,7 @@
 package raisetech.StudentManagement.domain;
 //Model
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,7 @@ import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentCourse;
 import java.util.List;
 
+@Schema(description = "受講生詳細")
 @Getter
 @Setter
 @NoArgsConstructor

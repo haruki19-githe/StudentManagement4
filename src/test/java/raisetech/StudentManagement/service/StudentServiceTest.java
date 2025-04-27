baaac902bb41b548712e33b737839f554504a69c
haruki19-githe <haruki_0120_fkt@icloud.com>
2025-04-27 18:47:46 +0900

    40_JUnitとAssertionとMockito_Serviceのテストの課題

--- src/test/java/raisetech/StudentManagement/service/StudentServiceTest.java
+++ src/test/java/raisetech/StudentManagement/service/StudentServiceTest.java
@@ -4,31 +4,39 @@ package raisetech.StudentManagement.service;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import raisetech.StudentManagement.converter.StudentConverter;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentCourse;
+import raisetech.StudentManagement.domain.StudentDetail;
 import raisetech.StudentManagement.repository.StudentRepository;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class StudentServiceTest {
 
+    @InjectMocks
+    StudentService studentService;
+
     @Mock
     private StudentRepository repository;
+
+    @Mock
+    private StudentDetail studentDetail;
 
     @Mock
     private StudentConverter converter;
 
     private StudentService sut;
 
+    //テストクラスの各テストメソッドの実行前に毎回実行される。
     @BeforeEach
     void before() {
         sut = new StudentService(repository, converter);
@@ -46,11 +54,35 @@ class StudentServiceTest {
         sut.searchStudentList();
 
         //検証
-        verify(repository, Mockito.times(1)).searchStudentList();
-        verify(repository, Mockito.times(1)).searchStudentCourseList();
-        verify(converter, Mockito.times(1)).convertStudentDetails(studentList, studentCourseList);
+        verify(repository, Mockito.times(1))
+                .searchStudentList();
+        verify(repository, Mockito.times(1))
+                .searchStudentCourseList();
+        verify(converter, Mockito.times(1))
+                .convertStudentDetails(studentList, studentCourseList);
 
 
     }
 
+    @Test
+    void 受講生詳細の検索＿リポジトリの処理が適切に呼び出せていること() {
+        String id="1";
+        Student student = new Student();
+        List<StudentCourse> studentCourse = new ArrayList<>();
+        when(repository.searchStudent(id)).thenReturn(student);
+        when(repository.searchStudentCourse(id)).thenReturn(studentCourse);
+
+        //実行
+        sut.searchStudentList();
+
+        //検証
+        verify(repository, Mockito.times(1))
+                .searchStudent(id);
+        verify(repository, Mockito.times(1))
+                .searchStudentCourse(id);
+        verify(studentDetail, Mockito.times(1))
+                .
+
+
+    }
 }

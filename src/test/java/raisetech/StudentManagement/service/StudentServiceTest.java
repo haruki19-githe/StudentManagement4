fcbadb2b858494862c9d27dc9810440b0a48b14a
haruki19-githe <haruki_0120_fkt@icloud.com>
2025-05-14 14:19:09 +0900

    40_JUnitとAssertionとMockito_Serviceのテストの課題の修正5回目（@BeforeEachの削除、InjectMocksの追加）

--- src/test/java/raisetech/StudentManagement/service/StudentServiceTest.java
+++ src/test/java/raisetech/StudentManagement/service/StudentServiceTest.java
@@ -2,7 +2,6 @@ package raisetech.StudentManagement.service;
 
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
